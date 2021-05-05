e72c6d3b146717d40c632c6f7ed5a585d660ac80
slondono19 <45316388+slondono19@users.noreply.github.com>
2021-05-05 13:39:54 -0700

    Added basic visual representation

--- calculatePi.js
+++ calculatePi.js
@@ -1,19 +1,26 @@
 function calculatePi(numberOfPoints){
+
+
     let pointsInCircle = 0;
     let pointsInSqure = 0;
 
     for(let i = 0; i<= numberOfPoints; i++){
         pointsInSqure += 1;
 
-        let numInx = Math.random();
-        let numInY = Math.random();
-        coordinates = Math.sqrt(numInY**2 + numInx**2);
-        
-        if(coordinates <= 1){
+        let numInX = Math.random()*600;
+        let numInY = Math.random()*600;
+        coordinates = Math.sqrt(numInY**2 + numInX**2);
+        ctx.fillRect(numInX,numInY,1,1);
+        if(coordinates <= 600){
             pointsInCircle += 1;
         }
     }
 
-    console.log((pointsInCircle*4)/(pointsInSqure));
+    return (pointsInCircle*4)/(pointsInSqure);
 }
 
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(300, 300, 300, 0, 2 * Math.PI);
+ctx.stroke();
