a1f28ce37e1dcdecb66e572ad2234cd7ed802c0b
slondono19 <45316388+slondono19@users.noreply.github.com>
2021-05-05 15:51:07 -0700

    added clear

--- calculatePi.js
+++ calculatePi.js
@@ -1,26 +1,52 @@
-function calculatePi(numberOfPoints){
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+function drawcircle(){
+ ctx.beginPath();
+ctx.arc(400, 400, 400, 0, 2 * Math.PI);
+ctx.stroke();   
+ctx.clearRect(0, 0, c.width, c.height);
+}
+
+drawcircle();
+
+const numberOfPoints = document.querySelector('.pointsYouWant');
+const tryButton = document.querySelector('.try');
+const showPi = document.querySelector('.piShow');
+let pi = 0;
 
 
+function calculatePi(points){
+    
     let pointsInCircle = 0;
     let pointsInSqure = 0;
+    
 
-    for(let i = 0; i<= numberOfPoints; i++){
+    drawcircle();
+    
+    for(let i = 0; i<= points; i++){
         pointsInSqure += 1;
-
-        let numInX = Math.random()*600;
-        let numInY = Math.random()*600;
+        
+        let numInX = Math.random()*800;
+        let numInY = Math.random()*800;
         coordinates = Math.sqrt(numInY**2 + numInX**2);
         ctx.fillRect(numInX,numInY,1,1);
-        if(coordinates <= 600){
+        if(coordinates <= 800){
             pointsInCircle += 1;
         }
     }
-
-    return (pointsInCircle*4)/(pointsInSqure);
+    
+    pi = (pointsInCircle*4)/(pointsInSqure);
+    //console.log(pi);
+    showPi.innerText = 'With '+ points+ ' points your pi calculation is: ' + pi;
 }
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(300, 300, 300, 0, 2 * Math.PI);
-ctx.stroke();
+
+
+
+//Event listeners
+
+tryButton.addEventListener('click', function(){
+    calculatePi(numberOfPoints.value);
+    numberOfPoints.value = '';
+});
